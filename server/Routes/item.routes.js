c3c93b09f26fb51a1423083223f489283fa4158a
pardhusnc2004 <pardupolisetty2018@gmail.com>
2025-03-01 00:11:40 +0530

    Temporarily hold admin auth for POST, PUT & DELETE Items

--- server/Routes/item.routes.js
+++ server/Routes/item.routes.js
@@ -6,8 +6,8 @@ const ItemRouter = Router();
 
 ItemRouter.get('/', GetAllItems)
 ItemRouter.get('/:id', GetItem)
-ItemRouter.post('/', Authenticate, AddItem)
-ItemRouter.put('/:id', Authenticate, UpdateItem)
-ItemRouter.delete('/:id', Authenticate, DeleteItem)
+ItemRouter.post('/', AddItem)
+ItemRouter.put('/:id', UpdateItem)
+ItemRouter.delete('/:id', DeleteItem)
 
 export default ItemRouter;
