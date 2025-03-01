d85e285c78cb38d6c341e656eb0ea684c1f7ab99
pardhusnc2004 <pardupolisetty2018@gmail.com>
2025-03-02 01:30:55 +0530

    Fix: Add Server's URL to CORS policy

--- server/Controllers/admin.controller.js
+++ server/Controllers/admin.controller.js
@@ -19,7 +19,7 @@ export const Login = async (req, res) => {
         res.cookie("jwt_secret", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "Lax",
+            sameSite: "None",
             maxAge: 24 * 60 * 60 * 1000,
         });
 
@@ -52,7 +52,7 @@ export const Login = async (req, res) => {
 
 export const Logout = async (req, res) => {
     try {
-        res.clearCookie("jwt_secret", { httpOnly: true, secure: true, sameSite: "Lax" });
+        res.clearCookie("jwt_secret", { httpOnly: true, secure: true, sameSite: "None" });
         return res.status(200).json({ message: "Logout successful..." });
     } catch (error) {
         return res.status(500).json({ message: error.message });
