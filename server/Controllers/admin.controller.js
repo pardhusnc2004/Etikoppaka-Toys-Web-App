3a90e96a6eabf92a53dc7c42bd2e173e2118d948
pardhusnc2004 <pardupolisetty2018@gmail.com>
2025-03-02 01:08:26 +0530

    Fix: Set sameSite attribute to "Lax" which is same as the cookie attirbute that was being set

--- server/Controllers/admin.controller.js
+++ server/Controllers/admin.controller.js
@@ -52,7 +52,7 @@ export const Login = async (req, res) => {
 
 export const Logout = async (req, res) => {
     try {
-        res.clearCookie("jwt_secret", { httpOnly: true, secure: true, sameSite: "strict" });
+        res.clearCookie("jwt_secret", { httpOnly: true, secure: true, sameSite: "Lax" });
         return res.status(200).json({ message: "Logout successful..." });
     } catch (error) {
         return res.status(500).json({ message: error.message });
